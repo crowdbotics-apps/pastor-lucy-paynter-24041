a79f9b987a949344ebebf0f8aa20514e2b6b60e2
Crowdbotics <team@crowdbotics.com>
2021-01-25 07:51:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pastor_lucy_paynter_24041/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pastor_lucy_paynter_24041;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "pastor_lucy_paynter_24041";
+    }
+}
---
+++ ios/pastor_lucy_paynter_24041Tests/pastor_lucy_paynter_24041Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface pastor_lucy_paynter_24041 : XCTestCase
+
+@end
+
+@implementation pastor_lucy_paynter_24041
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
